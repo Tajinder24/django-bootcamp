a23e624b64a94093305322e7e682f66a12cb945c
Tajinder24 <tajinderc4@gmail.com>
2021-02-24 21:28:15 +0530

    day 2 commit

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World!!</h1>")
+
+
+def product_detail_view(request, pk):
+    try:     
+        obj = Product.objects.get(id = pk)
+    except Product.DoesNotExist:
+        raise Http404 #Render html page with HTTP Status code of404
+    return HttpResponse(f"<h1> Product id - {obj.id} </h1>")
+
+class HomeView:
+    pass
